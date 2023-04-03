35e6963fa69f440522d878e90d7d1a2b17200daf
chenyinkai <287696322@qq.com>
2023-04-03 16:56:18 +0800

    feat: update default username

--- start.sh
+++ start.sh
@@ -3,9 +3,3 @@ cd ./service
 nohup pnpm start > service.log &
 echo "Start service complete!"
 
-
-cd ..
-echo "" > front.log
-nohup pnpm dev > front.log &
-echo "Start front complete!"
-tail -f front.log
